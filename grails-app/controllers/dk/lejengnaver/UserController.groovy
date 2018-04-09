3b11a50c8e7e710b740220765816ddf9ae278173
U-CORP\miandrea <miandrea@LNOR043094.corp.capgemini.com>
2018-04-09 11:27:33 +0200

    minor

--- grails-app/controllers/dk/lejengnaver/UserController.groovy
+++ grails-app/controllers/dk/lejengnaver/UserController.groovy
@@ -3,6 +3,9 @@ package dk.lejengnaver
 import grails.rest.RestfulController
 
 class UserController extends RestfulController {
+
+    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+
     static responseFormats = ['json', 'xml']
     UserController() {
         super(User)
--- src/test/groovy/dk/lejengnaver/UserControllerSpec.groovy
+++ src/test/groovy/dk/lejengnaver/UserControllerSpec.groovy
@@ -10,14 +10,33 @@ class UserControllerSpec extends HibernateSpec implements ControllerUnitTest<Use
         jsonSmartViewResolver(JsonViewResolver)
     }
 
-    def setup() {
+    void setup() {
+        User.saveAll(
+                new User(username: 'Adam Ant', password: "changeme", location: "All Sins"),
+                new User(username: 'Brian Bow', password: "changeme", location: "Bell Grove"),
+                new User(username: 'Carl Colt', password: "changeme", location: "Crank River"),
+                new User(username: 'Danny Deal', password: "changeme", location: "Ocean Drive"),
+        )
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-        true == true
+    void 'test the create action'() {
+        when: 'A query is executed that finds results by unique username'
+        User[] users = User.findAll("from User as u where u.username=?", ['Adam Ant'])
+
+        then: 'The response is correct'
+        users.size() == 1
+        users[0].location == 'All Sins'
     }
+
+    void 'test the search action finds results'() {
+        when: 'A query is executed that finds results by identical password'
+        User[] users = User.findAll("from User as u where u.password=?", ['changeme'])
+
+        then: 'The response is correct'
+        users.size() == 4
+    }
+
 }
